0e33de5437ec5795b9ddbc7fdfa581147958e9ab
Jonathan Liu <jonathan.liu@stjohn.org.au>
2022-08-10 10:10:22 +1000

    added middleware

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.port || 3000;
 
+/**
+ * Middlewares
+ */
+// Logger
+app.use(morgan('dev'));
+// CORS
+app.use(cors());
+// Body Parser
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('Post request received');
 });
 
 app.listen(port, () => {
