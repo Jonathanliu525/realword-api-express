31b74fd40ce603cb00b4af6b62a60964b1822913
Jonathan Liu <jonathan.liu@stjohn.org.au>
2022-08-16 10:04:40 +1000

    Extracted validator

--- router/auth.js
+++ router/auth.js
@@ -1,8 +1,6 @@
 const router = require('Express').Router();
 const authController = require('../controllers/authController');
-const { body, validationResult } = require('express-validator');
-
-const { User } = require('../model');
+const userValidator = require('../validator/user');
 
 //Authentication;
 router.post(
@@ -12,44 +10,6 @@ router.post(
 );
 
 //Registration
-router.post(
-  '/',
-  [
-    //Validate username
-    body('user.username')
-      .notEmpty()
-      .withMessage('Username is required')
-      .bail()
-      .custom(async (username) => {
-        const user = await User.findOne({ username });
-        if (user) {
-          throw new Error('Username already exists');
-        }
-      }),
-    //Validate password
-    body('user.password').notEmpty().withMessage('Password is required'),
-    //Validate email
-    body('user.email')
-      .notEmpty()
-      .withMessage('Email is required')
-      .isEmail()
-      .withMessage('Email is invalid')
-      .bail()
-      .custom(async (email) => {
-        const user = await User.findOne({ email });
-        if (user) {
-          throw new Error('Email already exists');
-        }
-      }),
-  ],
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
-  authController.register,
-);
+router.post('/', userValidator.register, authController.register);
 
 module.exports = router;
---
+++ validator/user.js
@@ -0,0 +1,36 @@
+const { body } = require('express-validator');
+const { validate } = require('../middleware/validator');
+const { User } = require('../model');
+
+const register = validate([
+  //Validate username
+  body('user.username')
+    .notEmpty()
+    .withMessage('Username is required')
+    .bail()
+    .custom(async (username) => {
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new Error('Username already exists');
+      }
+    }),
+  //Validate password
+  body('user.password').notEmpty().withMessage('Password is required'),
+  //Validate email
+  body('user.email')
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Email is invalid')
+    .bail()
+    .custom(async (email) => {
+      const user = await User.findOne({ email });
+      if (user) {
+        throw new Error('Email already exists');
+      }
+    }),
+]);
+
+module.exports = {
+  register,
+};
